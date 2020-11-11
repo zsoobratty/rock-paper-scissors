fd156fc3f6e76812f27764d2b862849b34d3a7b0
Ziad <ziad1991@hotmail.co.uk>
2020-11-11 13:13:26 +0000

    Cache DOM

---
+++ app.js
@@ -0,0 +1,10 @@
+let userScore = 0;
+let computerScore = 0;
+const userScoreEl = document.getElementById('user-score')
+const compScoreEl = document.getElementById('comp-score')
+const scoreBoardEl = document.querySelector('.scoreboard')
+const resultEl = document.querySelector('.result')
+const rockEl = document.getElementById('r')
+const paperEl = document.getElementById('p')
+const scissorsEl = document.getElementById('s')
+
