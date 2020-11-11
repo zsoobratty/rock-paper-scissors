7ab78db00a219f15cb7926491fff312555e24bd1
Ziad <ziad1991@hotmail.co.uk>
2020-11-11 13:30:23 +0000

    Functionality for computer choice

--- app.js
+++ app.js
@@ -8,3 +8,42 @@ const rockEl = document.getElementById('r')
 const paperEl = document.getElementById('p')
 const scissorsEl = document.getElementById('s')
 
+const handleCompChoice = () => {
+    const choices = ['r', 'p', 's']
+    const randomNum = (Math.floor(Math.random() * choices.length))
+    return choices[randomNum]
+}
+
+const playGame = (choice) => {
+    const computerChoice = handleCompChoice()
+    switch(choice + computerChoice) {
+        case('rs'):
+        case('pr'):
+        case('sp'):
+            console.log('User wins')
+            break;
+        case 'rp':
+        case 'ps':
+        case 'sr':
+            console.log('Comp wins')
+            break;
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            console.log("It's a tie!")
+    }
+
+}
+
+
+// Event Listeners
+
+rockEl.addEventListener('click', () => {
+    playGame('r')
+})
+paperEl.addEventListener('click', () => {
+    playGame('p')
+})
+scissorsEl.addEventListener('click', () => {
+    playGame('s')
+})
