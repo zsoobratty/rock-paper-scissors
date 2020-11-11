ae52157c1aed0b6fc1b85930122c56e42fc395ef
Ziad <ziad1991@hotmail.co.uk>
2020-11-11 13:49:54 +0000

    Functionality for playing game

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let userScore = 0;
-let computerScore = 0;
+let compScore = 0;
 const userScoreEl = document.getElementById('user-score')
 const compScoreEl = document.getElementById('comp-score')
 const scoreBoardEl = document.querySelector('.scoreboard')
-const resultEl = document.querySelector('.result')
+const resultEl = document.querySelector('.result > p')
 const rockEl = document.getElementById('r')
 const paperEl = document.getElementById('p')
 const scissorsEl = document.getElementById('s')
@@ -14,23 +14,46 @@ const handleCompChoice = () => {
     return choices[randomNum]
 }
 
-const playGame = (choice) => {
-    const computerChoice = handleCompChoice()
-    switch(choice + computerChoice) {
+const convertToWord = (letter) => {
+    if(letter === 'r') return 'Rock'
+    if(letter === 'p') return 'Paper'
+    if(letter === 's') return 'Scissors'
+}
+
+const winGame = (userChoice, compChoice) => {
+    userScore += 1
+    userScoreEl.innerHTML = userScore
+    resultEl.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(compChoice)}. You Win!`
+}
+
+const loseGame = (userChoice, compChoice) => {
+    compScore += 1
+    compScoreEl.innerHTML = compScore
+    resultEl.innerHTML = `${convertToWord(compChoice)} beats ${convertToWord(userChoice)}. You Lose!`
+}
+
+const drawGame = (userChoice) => {
+    resultEl.innerHTML = `You both picked ${convertToWord(userChoice)}. It's a tie!`
+}
+
+const playGame = (userChoice) => {
+    const compChoice = handleCompChoice()
+    switch(userChoice + compChoice) {
         case('rs'):
         case('pr'):
         case('sp'):
-            console.log('User wins')
+            winGame(userChoice, compChoice)
             break;
         case 'rp':
         case 'ps':
         case 'sr':
-            console.log('Comp wins')
+            loseGame(userChoice, compChoice)
             break;
         case 'rr':
         case 'pp':
         case 'ss':
-            console.log("It's a tie!")
+            drawGame(userChoice)
+            break;
     }
 
 }
